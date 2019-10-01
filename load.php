383ba56bfd4998e15f54885daed12d0a5840e150
Robert O'Rourke <robert@o-rourke.org>
2019-10-01 12:51:24 +0100

    Support new module loading style

--- load.php
+++ load.php
@@ -11,11 +11,6 @@ use function Altis\register_module;
 
 require_once __DIR__ . '/inc/namespace.php';
 
-// Do not initialise if plugin.php hasn't been included yet.
-if ( ! function_exists( 'add_action' ) ) {
-	return;
-}
-
 add_action( 'altis.modules.init', function () {
 	$default_settings = [
 		'enabled' => true,
